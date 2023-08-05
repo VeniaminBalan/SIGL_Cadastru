c9a6e9f5f4390672328aca2e9162ec0c067b02ba
Balan Veniamin <veniamin.balan02@gmail.com>
2023-08-05 16:20:24 +0300

    WIP

---
+++ SIGL_Cadastru.Repo/Models/DbNode.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Models;
+
+public class DbNode // TODO
+{
+    public Guid Id { get; set; }
+
+    [ForeignKey(nameof(DbNode))]
+    public Guid ParentId { get; set; }
+    public string Category { get; set; }
+    public string Name { get; set; }
+
+}
